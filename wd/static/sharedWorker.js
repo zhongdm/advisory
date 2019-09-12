91ae6dd2d389e17d43dd603f85c634734149b25c
仲冬梅 <z.dmnj@163.com>
2019-09-12 22:13:13 +0800

    advisory project

--- wd/static/sharedWorker.js
+++
@@ -1,7 +0,0 @@
-onconnect = function(event) {
-  let port = event.ports[0]
-  
-  port.onmessage = (e) => {
-    port.postMessage(+e.data[0] + +e.data[1])
-  }
-}
--- wd/static/worker.js
+++
@@ -1,9 +0,0 @@
-onmessage = function(event) {
-  let data = event.data
-  let ans = fibonacci(data)
-  postMessage(ans)
-}
-
-function fibonacci (n) {
-  return n < 2 ? n: fibonacci(n-1) + fibonacci(n-2)
-}
