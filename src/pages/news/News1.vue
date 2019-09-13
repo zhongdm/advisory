0c33ab8a423d0ad723e38f290efd5f18410db21b
huangcheng <huangcheng@zhisheng.ac.cn>
2019-09-13 22:34:04 +0800

    news1新增

---
+++ src/pages/news/News1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="news-container hot-container">
+    <h3>闻动增长研习社之南京天众增长主题研习活动圆满完成！</h3>
+    <section>
+      <p>2019年7月24日，在南京浦口区“不老传说”民宿酒店，闻动管理顾问（南京）有限公司为南京天众汽车销售服务有限公司开展了一次为期一天的“增长”主题培训。</p>
+      <article>
+        <p>“增长”的思维模型</p>
+        <p>
+          上午，闻动顾问创始人、首席增长官（CGO）毛雷老师分享了闻动顾问对企业盈利和增长的核心观点，读懂增长背后的关键成功因素“人性”，基于人性的“底层思维”重构企业增长思维。通过案例解析，拆解案例背后的增长思维模型和三法则。学员们通过参与案例研讨和设计增长方案，对增长的思维模型及三法则有了深刻的认知。
+        </p>
+        <img src="" alt="">
+        <span class="img-title">案例研讨中的“增长小组”</span>
+        <img src="" alt="">
+        <span class="img-title">南京天众业绩增长方案设计</span>
+        <p>“增长”方法论+刻意练习</p>
+        <p>下午，闻动顾问毛雷老师带领大家学习了业绩增长的方法论，推演了增长战略的“1+5”模型，并解构了亚马逊案例的增长飞轮模型，引导三个增长小组对公司的业务增长战略的创新路径和增长飞轮的设计。</p>
+        <img src="" alt="">
+        <span class="img-title">南京天众增长路径研讨</span>
+        <img src="" alt="">
+        <span class="img-title">南京天众增长飞轮设计</span>
+        <p>一天的培训活动圆满结束，南京天众公司李煜总经理总结表示，通过本次培训，帮大家撬开了新思维，换种思维去重新思考业务，是一次关于“增长”的深度解读，同时，通过大家的参与和体验，加深了大家对于“增长”逻辑的认知。</p>
+      </article>
+    </section>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.hot-container
+  h3
+    text-align center
+    margin 1rem auto
+  article
+    margin .5rem 0
+    h4
+      color #ab1942
+      font-weight bold
+      font-size 1.1rem
+    .img-title
+      text-align center
+      display block
+</style>
